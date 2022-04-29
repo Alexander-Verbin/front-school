7c730f38faf99608d735fcd753bca02d7bf78d95
Alexander-Verbin <sanek.verbin@mail.ru>
2022-04-29 19:17:02 +0300

    chenge resume

--- src/blocks/stitle/stitle.js
+++ src/blocks/stitle/stitle.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     $('.stitle__titles').click(function () {
-        $('.stitle__titles').toggleClass('stitle__titles--open')
+        $(this)('.stitle__titles').toggleClass('stitle__titles--open')
     })
 })
